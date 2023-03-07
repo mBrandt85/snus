e381cd1812e0ccdf5feb9949199be52204cff7ea
Magnus Brandt <magnusbrandt85@gmail.com>
2023-03-07 21:57:15 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/snus',
   plugins: [react()],
   build: {
     outDir: './build',
-    assetsDir: '.'
+    assetsDir: './',
   }
 })
