aec931decc6fcaab0b6541d86d2e63d8b504baae
Magnus Brandt <magnusbrandt85@gmail.com>
2023-03-07 21:26:09 +0100

    utrakning

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { NumberInput, Stack, Text, TextInput } from "@mantine/core"
+import { IconWeight } from "@tabler/icons-react"
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const firstSet = {
+  natriumChloride: 0.09,
+  water: 1.25,
+}
+
+const secondSet = {
+  sodiumCarbonate: 0.078,
+  glycerine: 0.045,
+  water: 0.3
+}
+
+export default function App() {
+  const [v, set] = useState('')
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{
+      margin: '0 auto',
+      width: '100%',
+      maxWidth: 400,
+      padding: 24
+    }}>
+      <Stack>
+        <TextInput
+          placeholder="Hur många gram tobak?"
+          radius="md"
+          size="lg"
+          rightSection={<IconWeight />}
+          onChange={e => set(e.target.value)}
+        />
+
+        {v && Number.isNaN(Number(v)) 
+          ? 'Fyll i tobak i nummer!'
+          : v.length > 0 && <>
+            <Text fz="lg">Steg 1</Text>
+            <Text fz="md">
+              Lös upp {Math.round(Number(v) * firstSet.natriumChloride)}g
+              natriumklorid i {Math.round(Number(v) * firstSet.water)}ml uppvärmt vatten.
+            </Text>
+            <Text fz="md">Blanda tobaken med saltvattnet.</Text>
+            <Text fz="md">Ställ i ugnen på 100 C i 35-40 timmar.</Text>
+            <Text fz="lg">Steg 2</Text>
+            <Text fz="md">Låt snuset kallna.</Text>
+            <Text fz="md">
+              Lös upp {Math.round(Number(v) * secondSet.sodiumCarbonate)}g
+              natriumkarbonat i {Math.round(Number(v) * secondSet.water)}ml uppvärmt vatten.
+            </Text>
+            <Text fz="md">
+              Blanda saltvattnet och {Math.round(Number(v) * secondSet.glycerine)}ml 
+              glycering med tobaken och blanda tills konsistensen är bra.
+            </Text>
+          </>
+        }
+      </Stack>
     </div>
   )
 }
-
-export default App
